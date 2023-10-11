486a7ad3bdaa51830d4bf47263008f3cb9067e80
S_HYEON <zxl3651@naver.com>
2023-10-11 22:00:02 +0900

    [Silver III] Title: N과 M (1), Time: 108 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.swift
@@ -0,0 +1,21 @@
+let input = readLine()!.split(separator: " ").map{Int($0)!}
+let (N,M) = (input[0], input[1])
+var answer : [Int] = []
+
+func dfs(){
+    if answer.count == M{
+        print(answer.map{String($0)}.joined(separator: " "))
+        return
+    } else {
+        for i in 1...N{
+            if !answer.contains(i){
+                answer.append(i)
+                dfs()
+                answer.removeLast()
+            }
+        }
+    }
+}
+
+dfs()
+
