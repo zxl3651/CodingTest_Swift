00df65cb632031362197d11a5eac88dedcc07853
S_HYEON <zxl3651@naver.com>
2024-01-03 22:26:39 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 924 ms, Memory: 399696 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.swift
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.swift
@@ -1,12 +1,15 @@
 import Foundation
 
-var s = Array(readLine()!)
+var s = ["-"] + readLine()!.map{ String($0) }
 let q = Int(readLine()!)!
+var arr = [[String: Int]](repeating: [:], count: s.count + 1)
 
+for i in 1..<s.count{
+    arr[i] = arr[i - 1]
+    arr[i][s[i], default: 0] += 1
+}
 for _ in 0..<q{
     let input = readLine()!.split(separator: " ")
-    let str = s[Int(input[1])!...Int(input[2])!]
-    print(str.reduce(0, {
-        String($1) == String(input[0]) ? $0 + 1 : $0
-    }))
+    let alpha = String(input[0]), l = Int(input[1])! + 1, r = Int(input[2])! + 1
+    print(arr[r][alpha, default: 0] - arr[l-1][alpha, default: 0])
 }
