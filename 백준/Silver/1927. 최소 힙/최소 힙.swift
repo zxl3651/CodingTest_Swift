5688eeb5de65d699800a5c8acb3df8bd8f244f40
S_HYEON <zxl3651@naver.com>
2024-03-09 17:42:44 +0900

    [Silver II] Title: 최소 힙, Time: 72 ms, Memory: 81080 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.swift
+++ 백준/Silver/1927. 최소 힙/최소 힙.swift
@@ -1,32 +1,71 @@
+struct Heap<T: Comparable> {
+    private var elements: [T] = []
+    
+    mutating func insert(element: T) {
+        if elements.isEmpty {
+            elements.append(element)
+            elements.append(element)
+            return
+        }
+        elements.append(element)
+        swimUp(index: elements.count - 1)
+    }
+    
+    mutating func swimUp(index: Int) {
+        var index = index
+        
+        while index > 1 && elements[index] < elements[index / 2] {
+            elements.swapAt(index, index / 2)
+            index /= 2
+        }
+    }
+    
+    mutating func pop() -> T? {
+        if elements.count < 2 { return nil }
+        
+        let deletedElement = elements[1]
+        elements.swapAt(1, elements.count - 1)
+        elements.removeLast()
+        
+        diveDown(index: 1)
+        return deletedElement
+    }
+    
+    mutating func diveDown(index: Int) {
+        let leftIndex = index * 2
+        let rightIndex = index * 2 + 1
+        var swapIndex = index
+        
+        if leftIndex <= elements.count - 1 && elements[leftIndex] < elements[swapIndex] {
+            swapIndex = leftIndex
+        }
+        
+        if rightIndex <= elements.count - 1 && elements[rightIndex] < elements[swapIndex] {
+            swapIndex = rightIndex
+        }
+        
+        if swapIndex != index {
+            elements.swapAt(index, swapIndex)
+            diveDown(index: swapIndex)
+        }
+        
+    }
+    
+}
+
 import Foundation
 
 var answer = ""
 
 let N = Int(readLine()!)!
-var heap = [Int]()
+var heap = Heap<Int>()
 
 for _ in 0..<N {
     let input = Int(readLine()!)!
     if input == 0 {
-        if heap.isEmpty {
-            answer += "0\n"
-        } else { // 큰 값이 있는경우
-            var min = Int.max, minIndex = 0
-            for i in 0..<heap.count {
-                if min > heap[i] {
-                    min = heap[i]
-                    minIndex = i
-                }
-            }
-            if min == Int.max {
-                answer += "0\n"
-                continue
-            }
-            heap[minIndex] = Int.max
-            answer += "\(min)\n"
-        }
-    } else { // 다른 숫자가 들어오는 경우
-        heap.append(input)
+        answer += "\(heap.pop() ?? 0)\n"
+    } else {
+        heap.insert(element: input)
     }
 }
 
